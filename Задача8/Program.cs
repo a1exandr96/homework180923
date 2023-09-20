3fcc390607dc11a983b15a52f3a4656e0b959bf0
al.one_cut@mail.ru <al.one_cut@mail.ru>
2023-09-20 17:11:27 +0300

    Добавили алгоритм решения Задачи 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter num");
+int a = int.Parse(Console.ReadLine());
+int res = 1;
+
+if (res == 1)
+
+{
+    res += 1;
+}
+
+while (res <= a)
+
+{
+
+    Console.Write(res + " ");
+    res += 2;
+}
