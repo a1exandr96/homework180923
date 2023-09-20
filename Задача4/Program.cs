9f0ec604c0cee5505cc44e11e52b4987b6a4bc22
al.one_cut@mail.ru <al.one_cut@mail.ru>
2023-09-20 16:59:52 +0300

    Длбавили алгоритм решения Задачи 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// // 2, 3, 7 -> 7
+// // 44 5 78 -> 78
+// // 22 3 9 -> 22
+
+Console.WriteLine("Enter 1st num");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 2nd num");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 3rd num");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+
+else if (max < c) 
+{
+    max = c;
+}
+
+Console.WriteLine("max = " + max);
